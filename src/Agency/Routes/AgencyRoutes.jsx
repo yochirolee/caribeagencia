10a7c340d66aebde92b10aa21c9cda68b513fca8
unknown <yleecruz@gmail.com>
2022-09-27 17:05:59 -0400

    react router dom navigate

--- src/Agency/Routes/AgencyRoutes.jsx
+++ src/Agency/Routes/AgencyRoutes.jsx
@@ -1,11 +1,18 @@
 import { React } from "react";
-import { Route, Routes,Navigate } from "react-router-dom";
-import { Dashboard } from "../Pages/Dashboard";
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import { DashboardLayout } from "../Layout/DashboardLayout";
+import { Inicio } from "../Pages/Inicio/Inicio";
+import { CreateOrders } from "../Pages//Orders/CreateOrders";
+import { ListOrders } from "../Pages/Orders/ListOrders";
 export const AgencyRoutes = () => {
 	return (
-		<Routes>
-			<Route path="/*" element={<Dashboard />} />
-		</Routes>
+		<DashboardLayout>
+			<Routes>
+				<Route path="/" element={<Inicio />} />
+				<Route path="create_order" element={<CreateOrders />} />
+				<Route path="list_orders" element={<ListOrders />} />
+				<Route path="/*" element={<Navigate to="/" />} />
+			</Routes>
+		</DashboardLayout>
 	);
 };
