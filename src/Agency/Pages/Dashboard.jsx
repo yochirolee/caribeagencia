629c788ffd7c7b6a6008332da480b8aed4297934
unknown <yleecruz@gmail.com>
2022-09-27 17:01:17 -0400

    react router dom navigate to

--- src/Agency/Pages/Dashboard.jsx
+++ src/Agency/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import { DashboardLayout } from "../Layout/DashboardLayout";
 import { Inicio } from "./Inicio/Inicio";
 import { CreateOrders } from "./Orders/CreateOrders";
@@ -8,9 +8,10 @@ export const Dashboard = () => {
 	return (
 		<DashboardLayout>
 			<Routes>
+				<Route path="/" element={<Inicio />} />
 				<Route path="create_order" element={<CreateOrders />} />
 				<Route path="list_orders" element={<ListOrders />} />
-				<Route path="/*" element={<Inicio />} />
+				<Route path="/*" element={<Navigate to="/" />} />
 			</Routes>
 		</DashboardLayout>
 	);
