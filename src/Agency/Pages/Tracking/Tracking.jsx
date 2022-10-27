8baea6da10f8968d8d916e64ab396b8f69bf643e
yochirolee <yleecruz@gmail.com>
2022-10-27 11:04:33 -0400

    qr-reader-add

--- src/Agency/Pages/Tracking/Tracking.jsx
+++ src/Agency/Pages/Tracking/Tracking.jsx
@@ -1,8 +1,23 @@
-import { React } from "react";
+import { React, useState } from "react";
+import { QrReader } from "react-qr-reader";
+
 export const Tracking = () => {
+	const [data, setData] = useState("No result");
 	return (
 		<>
-			<h1> Tracking</h1>
+			<QrReader
+				onResult={(result, error) => {
+					if (!!result) {
+						setData(result?.text);
+					}
+
+					if (!!error) {
+						console.info(error);
+					}
+				}}
+				style={{ width: "100%" }}
+			/>
+			<p>{data}</p>
 		</>
 	);
 };
