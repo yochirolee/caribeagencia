6f9f67b9dd3a91310f0c65ea4a9c23b4ee4e9ec1
yochirolee <yleecruz@gmail.com>
2022-10-27 11:19:44 -0400

    qr-reader-add facing mode select

--- src/Agency/Pages/Tracking/Tracking.jsx
+++ src/Agency/Pages/Tracking/Tracking.jsx
@@ -3,8 +3,14 @@ import { QrReader } from "react-qr-reader";
 
 export const Tracking = () => {
 	const [data, setData] = useState("No result");
+	const [selected, setSelected] = useState("environment");
+
 	return (
 		<>
+			<select onChange={(e) => setSelected(e.target.value)}>
+				<option value={"environment"}>Back Camera</option>
+				<option value={"user"}>Front Camera</option>
+			</select>
 			<QrReader
 				facingMode={"environment"}
 				delay={500}
