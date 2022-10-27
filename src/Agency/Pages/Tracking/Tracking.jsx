e350ac925d50e62f22be26373ffc4c5ffc08e6cf
yochirolee <yleecruz@gmail.com>
2022-10-27 16:29:56 -0400

    qr-reader-add find no data 96666 table

--- src/Agency/Pages/Tracking/Tracking.jsx
+++ src/Agency/Pages/Tracking/Tracking.jsx
@@ -32,7 +32,7 @@ export const Tracking = () => {
 		};
 		play();
 		let exist = items.find((item) => item.TrackingId == newItem.TrackingId);
-		setData(exist.TrackingId);
+		setData(exist?.TrackingId);
 
 		setItems((items) => [...items, newItem]);
 	};
