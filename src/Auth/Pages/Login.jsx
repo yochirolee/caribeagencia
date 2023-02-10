f6106b6d81bd0363b27eeea5e57186ceccb32497
yochirolee <yleecruz@gmail.com>
2023-02-10 09:56:29 -0500

    logo fix

--- src/Auth/Pages/Login.jsx
+++ src/Auth/Pages/Login.jsx
@@ -30,7 +30,7 @@ export const Login = () => {
 						href="#"
 						className="flex items-center gap-4 mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
 					>
-						<img className="w-16 h-16 object-contain " src="/public/ctelogo.png" alt="logo" />
+						<img className="w-16 h-16 object-contain " src="/ctelogo.png" alt="logo" />
 						Caribe Travel Express
 					</a>
 					<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
