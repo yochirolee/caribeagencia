5b6960c0a1e876a430ad4c77168abfda533f79a0
yochirolee <yleecruz@gmail.com>
2023-03-14 11:35:02 -0400

    report react icons 54d

--- src/Agency/Components/ui/Stats/ReportContainerStats.jsx
+++ src/Agency/Components/ui/Stats/ReportContainerStats.jsx
@@ -27,16 +27,16 @@ export const ReportContainerStats = ({ filteredProducts, selectedAgency }) => {
 
 	return (
 		<div className="container mt-6 flex  flex-wrap align-middle xl:grid xl:grid-cols-6 gap-4  p-4  bg-gray-50  ">
-			<Card decoration="top" decorationColor="gray">
+			<Card>
 				<Flex justifyContent="start" className="space-x-4">
-					<Icon icon={BarsArrowDownIcon} variant="light" size="sm" color="gray"/>
+					<Icon icon={BarsArrowDownIcon} variant="light" size="sm" color="gray" />
 					<div className="truncate">
 						<Text>No Facturas</Text>
 						<Metric className="text-xl">{uniqueInvoices.length}</Metric>
 					</div>
 				</Flex>
 			</Card>
-			<Card decoration="top" decorationColor="orange">
+			<Card >
 				<Flex justifyContent="start" className="space-x-4">
 					<Icon icon={FaBalanceScaleRight} variant="light" size="sm" color="orange" />
 					<div className="truncate ">
@@ -48,7 +48,7 @@ export const ReportContainerStats = ({ filteredProducts, selectedAgency }) => {
 				</Flex>
 			</Card>
 
-			<Card decoration="top">
+			<Card >
 				<Flex justifyContent="start" className="space-x-4">
 					<Icon icon={FaBalanceScaleRight} variant="light" size="sm" />
 					<div className="truncate ">
@@ -57,7 +57,7 @@ export const ReportContainerStats = ({ filteredProducts, selectedAgency }) => {
 					</div>
 				</Flex>
 			</Card>
-			<Card decoration="top" decorationColor="red">
+			<Card >
 				<Flex justifyContent="start" className="space-x-4">
 					<Icon icon={FaBalanceScaleRight} variant="light" size="sm" color="red" />
 					<div className="truncate ">
@@ -67,7 +67,7 @@ export const ReportContainerStats = ({ filteredProducts, selectedAgency }) => {
 				</Flex>
 			</Card>
 
-			<Card decoration="top" decorationColor="yellow">
+			<Card >
 				<Flex justifyContent="start" className="space-x-4">
 					<Icon icon={FaBalanceScaleRight} variant="light" size="sm" color="yellow" />
 					<div className="truncate ">
@@ -76,12 +76,12 @@ export const ReportContainerStats = ({ filteredProducts, selectedAgency }) => {
 					</div>
 				</Flex>
 			</Card>
-			<Card decoration="top" decorationColor={'green'}>
+			<Card >
 				<Flex justifyContent="start" className="space-x-4">
 					<Icon icon={FaDollarSign} variant="light" size="sm" color="green" />
 					<div className="truncate ">
 						<Text>Ganancia</Text>
-						<Metric  className="text-xl">$ {parseFloat(total - discount).toFixed(2)}</Metric>
+						<Metric className="text-xl">$ {parseFloat(total - discount).toFixed(2)}</Metric>
 					</div>
 				</Flex>
 			</Card>
